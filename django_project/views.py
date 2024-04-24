5b1c7aa2a1c76d167abf276d782ecdc2fb3d367e
Githinji Halifax <halifaxhali77@gmail.com>
2024-04-24 12:59:32 +0000

    Intergrated random  APIs with random content

--- django_project/views.py
+++ django_project/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+import requests
 def index(request):
-  return render(request, 'templates/index.html')
+  r1=requests.get('https://api.github.com/events')
+  data = r1.json()
+  events = data[0]["id"]
+  
+                    
+  return render(request,'templates/index.html',{'events':events})
